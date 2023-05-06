e2f37c765847083280a6e800afe274a1b0fcc426
Stephen Pearce <hello@stevepdp.dev>
2023-05-06 21:59:43 +0100

    feat(emotionengine): minor tidy of rotation script

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    private float rotateSpeed = 0.25f;
+    [SerializeField] float rotateSpeed = .25f;
 
     void Update()
+    {
+        RotateOnYAxis();
+    }
+
+    void RotateOnYAxis()
     {
         transform.Rotate(0, rotateSpeed, 0, Space.World);
     }
